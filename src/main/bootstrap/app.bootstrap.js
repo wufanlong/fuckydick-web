d61c6c2d05a4414c3104d5d22747815c154b88a4
wufanlong <14043878+wufanlong@user.noreply.gitee.com>
2026-01-02 02:41:51 +0800

    应用启动时自动安装局域网扫描所需组件

--- src/main/bootstrap/app.bootstrap.js
+++ src/main/bootstrap/app.bootstrap.js
@@ -1,19 +1,18 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { exec } = require('child_process');
-const regedit = require('regedit');
 
 
 // 执行 npcap 安装程序
 function runNpcapInstaller() {
     // 注册表中查找是否有相应的值
-    regedit.list('HKLM\\SOFTWARE\\WOW6432Node\\Npcap', (err, result) => {
+    exec('reg query "HKLM\\SOFTWARE\\WOW6432Node\\Npcap"', (err, result) => {
         const install = () => {
             let installerPath;
             if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
                 installerPath = path.join(__dirname, '../../extra/npcap-1.85.exe');
             } else {
-                installerPath = path.join(__dirname, 'resources', 'npcap-1.85.exe')
+                installerPath = path.join(process.resourcesPath, 'npcap-1.85.exe')
             }
             exec(installerPath, { 'shell': 'powershell.exe' }, (err, stdout, stderr) => {
                 if (err) {
@@ -24,13 +23,16 @@ function runNpcapInstaller() {
             })
         }
         if (err) {
-            console.error(`Error: ${err}`);
-            install()
+            if (err.code === 1) {
+                console.log('未找到指定注册表路径')
+                install()
+            } else if (err.code === 5) {
+                console.error('无权限访问指定注册表路径')
+            } else {
+                console.error("未知错误code=" + err.code)
+            }
         }
-        if (!result['HKLM\\SOFTWARE\\WOW6432Node\\Npcap'].exists) {
-            install()
-        }
-    });
+    })
 };
 
 app.whenReady().then(() => {
